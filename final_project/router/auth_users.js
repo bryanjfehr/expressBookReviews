2340a69de71a90c49c2e610d86b317e990a58458
bryanjfehr <bryanjfehr@gmail.com>
2024-12-11 13:16:44 -0500

    added auth user logic

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,23 +6,106 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    let userswithsamename = users.filter((user)=>{
+        return user.username === username
+      });
+      if(userswithsamename.length > 0){
+        return true;
+      } else {
+        return false;
+      }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+      });
+      if(validusers.length > 0){
+        return true;
+      } else {
+        return false;
+      }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+  
+    if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+  
+      req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const lookupISBN = req.params.isbn;
+    const bookList = Object.values(books).find(book => book.ISBN === lookupISBN);
+
+    if (bookList) {
+        const reviews = bookList.reviews || {};
+        const username = req.session.authorization && req.session.authorization.username;
+
+        if (username) {
+            const userReview = req.body.review;
+
+            if (userReview) {
+                if (reviews[username]) {
+                    reviews[username] = userReview;
+                    res.send({ message: `Review modified by ${username}` });
+                } else {
+                    reviews[username] = userReview;
+                    res.send({ message: `New review by ${username}` });
+                }
+            } else {
+                res.status(400).json({ message: "Review not found" });
+            }
+        } else {
+            res.status(403).json({ message: "No authentication detected. Please log in." });
+        }
+    } else {
+        res.status(404).json({ message: `No book found with ISBN ${lookupISBN}` });
+    }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const lookupISBN = req.params.isbn;
+    const bookList = Object.values(books).find(book => book.ISBN === lookupISBN);
+
+    if (bookList) {
+        const reviews = bookList.reviews || {};
+        const username = req.session.authorization && req.session.authorization.username;
+        if (username) {
+            const userReview = req.body.review;
+            if (userReview) {
+                if (reviews[username]) {
+                    delete reviews[username]
+                    res.send({ message: `Review deleted by ${username}` });
+                } else {
+                    res.send({ message: `No reviews by ${username}` });
+                }
+            } else {
+                res.status(403).json({ message: "No review to delete." });
+            }
+        } else {
+            res.status(403).json({ message: "No authentication detected. Please log in." });
+        }
+    } else {
+        res.status(404).json({ message: `No book found with ISBN ${lookupISBN}` });
+    }
 });
 
 module.exports.authenticated = regd_users;
